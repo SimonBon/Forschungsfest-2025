ace9f25534a45cb9701e2703fbb74da47ce91902
SimonBon <simon.bonvino@gmail.com>
2025-03-18 11:06:26 +0100

    added change of last_checkpoint

--- GUI/zenodo_utils.py
+++ GUI/zenodo_utils.py
@@ -69,6 +69,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     out_path = Path(args.out_path).resolve()
+    print(out_path)
     
     record_id = "15040813"  # Replace with your Zenodo record ID
     filename = 'data.zip'  # Replace with the exact filename you wish to download
